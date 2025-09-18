ca48367ba4a87ef0585e8ef5dea4b0a79798cd19
LucaolDev <labsfiap@fiap.com.br>
2025-09-17 22:45:42 -0300

    Atualização

--- src/main/java/br/fiap/teste/controller/TesteController.java
+++ src/main/java/br/fiap/teste/controller/TesteController.java
@@ -1,9 +1,12 @@
 package br.fiap.teste.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
@@ -14,5 +17,18 @@ public class TesteController {
     @GetMapping("/saudacao")
     public Map<String, String> saudacao(@RequestParam(defaultValue = "Fiap") String nome){
         return Map.of("Mensagem", "Olá " + nome);
+    }
+
+    @GetMapping("/somar")
+    public Map<String, Integer> somar (@RequestParam int a, @RequestParam int b) {
+        return Map.of("resultado", a+b);
+    }
+
+    @GetMapping("/dividir")
+    public Map<String, Double> dividir(@RequestParam int a, @RequestParam int b) {
+        if (b == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Não existe divisão por zero!");
+        }
+        return Map.of("resultado", (double) (a/b));
     }
 }
